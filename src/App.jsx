e5e85844cec2fba99f04db9a2b73fa5d9f1e2895
Ctrlalton03 <altonsimpson22@gmail.com>
2025-07-08 13:06:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to My App</h1>
+      <p> This is a simple React application called Food Finder.</p>
+    </>
+  )
+}
+
+export default App
