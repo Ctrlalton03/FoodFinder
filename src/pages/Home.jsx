36c83716da4eac7f9698665947e37eecf02650c0
Alton Simpson <altonsimpson22@gmail.com>
2025-07-12 12:33:14 -0400

    got rid of app.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
-
+import '../Css/Home.css';
 
 const Home = () => {
    
     return(
         <>
-         <h1>FoodFinder</h1>
-         <p>Welcome to Food Finder, your go-to web app for discovering delicious food options!</p>
-         <div className="foodfinder-description">
-            <p>Here, you can search for food items by name or category.</p>
-            <p>Explore a wide variety of food choices and find your next meal with ease.</p>
-            <p>Whether you're looking for a specific dish or just browsing, Food Finder has got you covered!</p>
-         </div>
+        <div className='home-container'>
+            <h1>FoodFinder</h1>
+            <p>Welcome to Food Finder, your go-to web app for discovering delicious food options!</p>
+            <div className="foodfinder-description">
+                <p>Here, you can search for food items by name or category.</p>
+                <p>Explore a wide variety of food choices and find your next meal with ease.</p>
+                <p>Whether you're looking for a specific dish or just browsing, Food Finder has got you covered!</p>
+            </div>
+        </div>
         </>
     )
 }
